edb8eb89b7d55d948947f4e371e94f4506571308
Michel Sievers <michel@easy-target.eu>
2019-08-20 23:31:40 +0200

    Started loop structure in network source.

--- giphytrends/giphytrends/GIFItemCollection/Provider/Network/GIFItemCollectionNetworkSource.swift
+++ giphytrends/giphytrends/GIFItemCollection/Provider/Network/GIFItemCollectionNetworkSource.swift
@@ -5,25 +5,39 @@ class GIFItemCollectionNetworkSource: NSObject, GIFItemCollectionSource {
     @IBOutlet weak var apiKeyStorage: NetworkSourceAPIKeyStorage!
     @IBOutlet weak var configuration: NetworkSourceConfiguration!
     
-    func getCollection(
-        delegate: GIFItemCollectionSourceDelegate?
-    ) {
-        let numberOfItemsPerPage = configuration.numberOfItemsPerPage
-        let numberOfPages = configuration.numberOfPages
-        
-        getCollection(
-            numberOfItemsPerPage: numberOfItemsPerPage,
-            numberOfPages: numberOfPages,
-            delegate: delegate
-        )
+    private var numberOfItemsPerPage: Int {
+        get {
+            return configuration.numberOfItemsPerPage
+        }
     }
+    private var numberOfPages: Int {
+        get {
+            return configuration.numberOfPages
+        }
+    }
+    private weak var delegate: GIFItemCollectionSourceDelegate?
+    private var collection: GIFItemCollection!
     
     func getCollection(
-        numberOfItemsPerPage: Int,
-        numberOfPages: Int,
         delegate: GIFItemCollectionSourceDelegate?
     ) {
-        weak var delegate = delegate
+        self.delegate = delegate
+        collection = GIFItemCollection()
         
+        for pageIndex in 0 ..< numberOfPages {
+            getPage(pageIndex: pageIndex)
+        }
+    }
+    
+}
+
+// MARK: - Implementations
+
+extension GIFItemCollectionNetworkSource {
+    
+    private func getPage(pageIndex: Int) {
+        let pageOffset = pageIndex * numberOfItemsPerPage
+        
+        delegate?.collectionSource(self, didGetCollection: collection)
     }
 }
