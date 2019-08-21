46cdf04423523d412bb4e23e2926272c0fb0d195
Michel Sievers <michel@easy-target.eu>
2019-08-21 19:09:07 +0200

    First impl of GIFItemCollectionNetworkResponse codable structs.

---
+++ giphytrends/GIFItemCollection/Provider/Network/NetworkResponseGIFItem.swift
@@ -0,0 +1,22 @@
+struct GIFItemCollectionNetworkResponse: Codable {
+    
+    let data: [GIFItemNetworkResponse]
+    
+    struct GIFItemNetworkResponse: Codable {
+        
+        let url: String
+        let images: GIFItemImagesNetworkResponse
+        
+        struct GIFItemImagesNetworkResponse: Codable {
+            
+            let fixed_height_small: GIFItemImageNetworkResponse
+            
+            struct GIFItemImageNetworkResponse: Codable {
+                
+                let url: String
+                
+            }
+        }
+    }
+
+}
