b571fa8aa3e0d12de988fa4e772a3bd49b29cfd8
Michel Sievers <michel@easy-target.eu>
2019-08-22 19:18:26 +0200

    Added Dynamic view binding.

---
+++ giphytrends/Common/ViewBinding/Dynamic.swift
@@ -0,0 +1,17 @@
+// Modified version of Mohammad Azam's Dynamic type
+// https://medium.com/@azamsharp/mvvm-in-ios-from-net-perspective-580eb7f4f129
+
+class Dynamic<T> {
+    
+    var bind: (T) -> () = { _ in }
+    
+    var value: T? {
+        didSet {
+            bind(value!)
+        }
+    }
+    
+    init(_ v: T) {
+        value = v
+}
+
--- giphytrends/UI/ViewModel/ItemCollectionViewModel.swift
+++ giphytrends/UI/ViewModel/ItemCollectionViewModel.swift
@@ -3,5 +3,11 @@ import Foundation
 class ItemCollectionViewModel: NSObject {
     
     @IBOutlet weak var provider: ItemCollectionProvider!
-
+    private let collectionWrapper = Dynamic(ItemCollection())
+    
+    func fetchEntireCollection() {
+        provider.getCollectionFromFastestSource { (collection) in
+            self.collectionWrapper.value = collection
+        }
+    }
 }
